5f4404c9b1e18402a744ada5648b84ef351eef1a
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-18 16:12:26 +0200

    apertura cartella lavoro

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/*
+---PROBLEMA---
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+// INTESTAZIONE VUE
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return{
+
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
